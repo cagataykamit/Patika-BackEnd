c184de957e8d0748130000b1de712df75b994d2f
cagataykamit <98674243+cagataykamit@users.noreply.github.com>
2022-02-02 12:25:11 +0300

    Add files via upload

---
+++ Calisma6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calisma6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >=6 && time <11)
+            {
+                Console.WriteLine("Günaydın1");
+            }
+            else if(time <=18)
+            Console.WriteLine("İyi Günler1");
+            else
+            Console.WriteLine("iyi Geceler1");
+
+            string sonuc = time<=18 ?"İyi günler2" : "İyi geceler2";
+            Console.WriteLine(sonuc);
+
+            sonuc = time>=6 && time <=11 ? "Günaydın3" : time<=18 ? "İyi günler3" : "İyi geceler3";
+            Console.WriteLine(sonuc);
+            
+        }
+    }
+}
