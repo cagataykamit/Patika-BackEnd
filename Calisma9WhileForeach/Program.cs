0cd11cd3b5143a2ae99271e2ada7d7cd9d452377
cagataykamit <98674243+cagataykamit@users.noreply.github.com>
2022-02-03 15:08:54 +0300

    Add files via upload

---
+++ Calisma9WhileForeach/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calisma9WhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac =1;
+            int toplam = 0;
+            while(sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+
+            char character='a';
+            while(character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            Console.Write("Foreach**** ");
+            string[] arabalar = {" BMW","FORD","TOYOTA","NİSSAN"};
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
