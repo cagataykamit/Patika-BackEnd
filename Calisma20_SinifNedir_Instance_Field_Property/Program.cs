fea39916cad07b1fcb065e8ee11b5e3c31016cc0
Çağatay Kamit <98674243+cagataykamit@users.noreply.github.com>
2022-02-27 21:24:28 +0300

    Add files via upload

---
+++ Calisma20_SinifNedir_Instance_Field_Property/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Calisma20_SinifNedir_Instance_Field_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad ="Ayşe";
+            calisan1.Soyad="Kara";
+            calisan1.No=23415634;
+            calisan1.Departman="İnsan Kaynakları";
+            calisan1.CalisanBilgileri();
+            Console.WriteLine("***********");
+
+
+            Calisan calisan2=new Calisan();
+            calisan2.Ad="Deniz";
+            calisan2.Soyad="Arda";
+            calisan2.No= 25646789 ;
+            calisan2.Departman= "Satın Alma";
+
+            calisan2.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan adı : {0}",Ad);
+            Console.WriteLine("Çalışan soyadı : {0} ",Soyad);
+            Console.WriteLine("Çalışanın Numarası : {0} ",No);
+            Console.WriteLine("Çalışanın Depertmanı : {0} ",Departman);
+        }
+    }
+}
